bb597ba876841d2c9a1cc43f808253656c8982b5
Ruslan Bayramov <ruslannbayramovvv@gmail.com>
2024-05-31 22:21:08 +0400

    CC and loading

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,5 +1,22 @@
-function Page() {
-  return <h1>Cabins page</h1>;
+import Counter from "../components/Counter";
+
+async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={data} />
+    </div>
+  );
 }
 
 export default Page;
